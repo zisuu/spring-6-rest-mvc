69de3df55dbbfb3fc7f7e2d67677f00e9b54e721
zisuu <david.ziswiler@finecloud.ch>
2023-04-04 21:25:54 +0200

    lombok constructors

---
+++ src/main/java/ch/finecloud/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,11 @@
+package ch.finecloud.spring6restmvc.controller;
+
+import ch.finecloud.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+}
