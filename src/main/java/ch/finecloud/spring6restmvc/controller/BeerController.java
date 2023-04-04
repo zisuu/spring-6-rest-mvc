a79d9cf6fef536bd389d7d05d0b9a186dcf6c8ae
zisuu <david.ziswiler@finecloud.ch>
2023-04-04 21:43:55 +0200

    lombok logging

--- src/main/java/ch/finecloud/spring6restmvc/controller/BeerController.java
+++ src/main/java/ch/finecloud/spring6restmvc/controller/BeerController.java
@@ -1,11 +1,24 @@
 package ch.finecloud.spring6restmvc.controller;
 
+import ch.finecloud.spring6restmvc.model.Beer;
 import ch.finecloud.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
+import java.util.UUID;
+
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
     private final BeerService beerService;
+
+    public Beer getBeerById(UUID id) {
+
+        log.debug("getBeerById was called with id: " + id + ", in Controller");
+
+        return beerService.getBeerById(id);
+    }
+
 }
--- src/main/java/ch/finecloud/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/ch/finecloud/spring6restmvc/services/BeerServiceImpl.java
@@ -2,14 +2,21 @@ package ch.finecloud.spring6restmvc.services;
 
 import ch.finecloud.spring6restmvc.model.Beer;
 import ch.finecloud.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("getBeerById was called with id: " + id + ", in Service");
+
         return Beer.builder()
                 .id(id)
                 .version(1)
