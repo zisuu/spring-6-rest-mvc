d0744f01d17f240b3e72b8c69b75112c041dfc6b
zisuu <david.ziswiler@finecloud.ch>
2023-04-06 21:10:42 +0200

    using json matcher

--- src/test/java/ch/finecloud/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/ch/finecloud/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package ch.finecloud.spring6restmvc.controller;
 
+import ch.finecloud.spring6restmvc.model.Beer;
 import ch.finecloud.spring6restmvc.services.BeerService;
 import ch.finecloud.spring6restmvc.services.BeerServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -11,9 +12,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
@@ -28,9 +31,13 @@ class BeerControllerTest {
 
     @Test
     void getBeerById() throws Exception {
-        mockMvc.perform(get("/api/v1/beers/" + UUID.randomUUID())
-                .accept(MediaType.APPLICATION_JSON))
+        Beer testBeer = beerServiceImpl.listBeers().get(0);
+        given(beerService.getBeerById(testBeer.getId())).willReturn(testBeer);
+        mockMvc.perform(get("/api/v1/beers/" + testBeer.getId())
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(testBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(testBeer.getBeerName().toString())));
     }
 }
