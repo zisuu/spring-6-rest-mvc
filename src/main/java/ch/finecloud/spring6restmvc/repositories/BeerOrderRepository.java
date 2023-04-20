3f004fa433a514406e89e6e6d1315065c524de25
zisuu <david.ziswiler@finecloud.ch>
2023-04-20 20:05:08 +0200

    add BeerOrderRepository

---
+++ src/main/java/ch/finecloud/spring6restmvc/repositories/BeerOrderRepository.java
@@ -0,0 +1,9 @@
+package ch.finecloud.spring6restmvc.repositories;
+
+import ch.finecloud.spring6restmvc.entities.BeerOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerOrderRepository extends JpaRepository<BeerOrder, UUID> {
+}
