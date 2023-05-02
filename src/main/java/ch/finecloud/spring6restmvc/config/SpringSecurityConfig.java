d9271569aba864f05c3fbc58d36529433e1931f5
zisuu <david.ziswiler@finecloud.ch>
2023-05-02 21:26:43 +0200

    add openapi validation example test

--- src/main/java/ch/finecloud/spring6restmvc/config/SpringSecurityConfig.java
+++ src/main/java/ch/finecloud/spring6restmvc/config/SpringSecurityConfig.java
@@ -2,9 +2,11 @@ package ch.finecloud.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
     @Bean
---
+++ src/test/java/ch/finecloud/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -0,0 +1,64 @@
+package ch.finecloud.spring6restmvc.controller;
+
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.restassured.OpenApiValidationFilter;
+import com.atlassian.oai.validator.whitelist.ValidationErrorsWhitelist;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
+
+import static com.atlassian.oai.validator.whitelist.rule.WhitelistRules.messageHasKey;
+import static io.restassured.RestAssured.given;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(BeerControllerRestAssuredTest.TestConfig.class)
+@ComponentScan(basePackages = "ch.finecloud.spring6restmvc")
+public class BeerControllerRestAssuredTest {
+
+    OpenApiValidationFilter filter = new OpenApiValidationFilter(OpenApiInteractionValidator
+            .createForSpecificationUrl("oa3.yml")
+            .withWhitelist(ValidationErrorsWhitelist.create()
+                    .withRule("Ignore date format",
+                            messageHasKey("validation.response.body.schema.format.date-time")))
+            .build());
+
+    @Configuration
+    public static class TestConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+            http.authorizeHttpRequests().anyRequest().permitAll();
+
+            return http.build();
+        }
+    }
+
+    @LocalServerPort
+    Integer localPort;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = localPort;
+    }
+
+    @Test
+    void testListBeers() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .filter(filter)
+                .get("/api/v1/beers")
+                .then()
+                .assertThat().statusCode(200);
+    }
+}
